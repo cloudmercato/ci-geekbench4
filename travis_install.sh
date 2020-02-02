35535d0bfc38d9946d2b4a1bf02801071d4b8d7e
Anthony Monthe <anthony.monthe@gmail.com>
2020-02-02 20:02:33 +0000

    Added Wercker

--- travis_install.sh
+++ travis_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]] ; then
-    wget -q http://cdn.geekbench.com/Geekbench-4.1.3-Linux.tar.gz
-    tar xf Geekbench-4.1.3-Linux.tar.gz
-    ln -s $(pwd)/Geekbench-4.1.3-Linux/geekbench4 ~/geekbench4
+    ./linux_install.sh
 elif [[ "$TRAVIS_OS_NAME" == "osx" ]] ; then
     wget -q http://cdn.geekbench.com/Geekbench-4.4.2-Mac.zip
     unzip -o -q Geekbench-4.4.2-Mac.zip
